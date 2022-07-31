48d5f8d64a95157fe78fb7a4bafb3db9a88537c3
Sandali Manimendra <manimendrasp.19@uom.lk>
2022-08-01 01:59:33 +0530

    Expenses Component

---
+++ src/Components/Expenses/Expenses.js
@@ -0,0 +1,32 @@
+import Card from '../UI/Card';
+import ExpenseItem from './ExpenseItem';
+import './Expenses.css';
+
+const Expenses = (props) => {
+  return (
+    <Card className="expenses">
+      <ExpenseItem
+        title={props.item[0].title}
+        date={props.item[0].date}
+        amount={props.item[0].amount}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={props.item[1].title}
+        date={props.item[1].date}
+        amount={props.item[1].amount}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={props.item[2].title}
+        date={props.item[2].date}
+        amount={props.item[2].amount}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={props.item[3].title}
+        date={props.item[3].date}
+        amount={props.item[3].amount}
+      ></ExpenseItem>
+    </Card>
+  );
+};
+
+export default Expenses;
